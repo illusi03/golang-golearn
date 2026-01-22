d2377831fa8a494142a08ad764ebd79a73d8d2e1
illusi03 <mohammad.azhari12@gmail.com>
2026-01-22 23:08:03 +0700

    Add FE

---
+++ template/dist/assets/___vite-browser-external_commonjs-proxy-BttVsNON-ySylaXa5.js
@@ -0,0 +1,2 @@
+import{e}from"./entry.client-Cy9IsQOv.js";const t={},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(o);export{a};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-BttVsNON-ySylaXa5.js.map
